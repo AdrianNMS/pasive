5813ca0ac867f39201c319a7eee9dcac19e292a9
Adrian Norvic Melgarejo Siguas <amelgars@emeal.nttdata.com>
2022-08-05 20:13:24 -0500

    first commit

---
+++ src/main/java/com/bank/pasive/PasiveApplication.java
@@ -0,0 +1,13 @@
+package com.bank.pasive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/pasive/PasiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.pasive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
