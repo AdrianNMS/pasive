021563e6a4768c76378f5a45946f1a786e8b25a6
AdrianNMS <adrian.n.melgarejo.s@gmail.com>
2022-08-15 11:12:22 -0500

    Added eureka client

--- src/main/java/com/bank/pasive/PasiveApplication.java
+++ src/main/java/com/bank/pasive/PasiveApplication.java
@@ -2,10 +2,11 @@ package com.bank.pasive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
-
+@EnableEurekaClient
 @SpringBootApplication
 public class PasiveApplication {
 	public static void main(String[] args) {
